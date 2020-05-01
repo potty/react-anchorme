450405aca5246583004970364537fc6ec8825a1d
Pavel Potáček <potacek.pavel@gmail.com>
2020-05-01 16:31:19 +0200

    Improve build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,14 @@
 import typescript from 'rollup-plugin-typescript2'
+import sizes from 'rollup-plugin-sizes'
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 export default {
 	input: 'src/index.ts',
 	output: [
-		{
-			file: pkg.module,
-			format: 'es',
-		},
+		{ file: pkg.main, format: 'cjs' },
+		{ file: pkg.module, format: 'es' },
 	],
 	external: ['react', 'anchorme'],
-	plugins: [
-		typescript({
-			typescript: require('typescript'),
-		}),
-		terser(),
-	],
+	plugins: [typescript(), terser(), sizes()],
 }

7ff425629e6a454b93dd6e579e8a3851c82235b1
Pavel Potáček <potacek.pavel@gmail.com>
2026-03-25 10:59:18 +0100

    refactor: anchorme component

--- src/Anchorme.tsx
+++ src/Anchorme.tsx
@@ -12,36 +12,33 @@ type Props = {
 const Anchorme = ({ children, ...rest }: Props) => {
 	const text = Array.isArray(children) ? children.join('') : children
 
-	const parsedText = useMemo(() => {
-		const matches = anchorme.list(text)
-		if (matches.length === 0) return text
+	const matches = useMemo(() => anchorme.list(text), [text])
 
-		const elements = []
-		let lastIndex = 0
-		matches.forEach((match) => {
-			// Push text located before matched string
-			if (match.start > lastIndex) {
-				elements.push(text.substring(lastIndex, match.start))
-			}
+	if (matches.length === 0) return <>{text}</>
 
-			// Push Link component
-			elements.push(
-				<Link {...rest} key={`link-${match.start}`} href={match.string} />,
-			)
-
-			lastIndex = match.end
-		})
-
-		// Push remaining text
-		if (text.length > lastIndex) {
-			elements.push(text.substring(lastIndex))
+	const elements = []
+	let lastIndex = 0
+	matches.forEach((match) => {
+		// Push text located before matched string
+		if (match.start > lastIndex) {
+			elements.push(text.substring(lastIndex, match.start))
 		}
 
-		return elements.length === 1 ? elements[0] : elements
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [text])
+		// Push Link component
+		elements.push(
+			<Link {...rest} key={`link-${match.start}`} href={match.string} />,
+		)
 
-	return <>{parsedText}</>
+		lastIndex = match.end
+	})
+
+	// Push remaining text
+	if (text.length > lastIndex) {
+		elements.push(text.substring(lastIndex))
+	}
+
+	const result = elements.length === 1 ? elements[0] : elements
+	return <>{result}</>
 }
 
 export default React.memo(Anchorme)
